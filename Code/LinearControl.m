91254ef44fbaeeae47173620396e07a3c8c35d41
rohigovi <43045693+rohigovi@users.noreply.github.com>
2019-03-31 18:24:51 -0700

    Update LinearControl.m

--- Code/LinearControl.m
+++ Code/LinearControl.m
@@ -4,7 +4,7 @@ instrreset
 clc
 
 error=heat_mat-ideal_heatmat;
-%Doing a control by varying amount that the valve is compressed
+
 
 k1=0.5;
 k2=0.5;
@@ -12,8 +12,12 @@ k3=0.5;
 servo1=0;
 servo2=0;
 servo3=0;
+
+% ADD PROPORTIONAL CONTROL FOR PUMP.
 s = serial('COM9','BAUD',9600); %Conflicting with temperature reading?
-if activate(1)
+if activate(1) 
+%Checking if control for valve1 should be activated
+%CHANGE THIS COMPLETELY. IMPLEMENT LOGIC FROM THE ARDUINO INTERFACE CODE.
     while(rms(error(1:3,1))>threshold1)
     servo1=servo1+k1*mean(error(1:3,1));
     fopen(s)
@@ -22,6 +26,10 @@ if activate(1)
     
 end
 if activate(2)
+%Checking if control for valve1 should be activated
+
+%CHANGE THIS COMPLETELY. IMPLEMENT LOGIC FROM THE ARDUINO INTERFACE CODE.
+
     while(rms(error(1:3,2))>threshold2)
     servo2=servo2+k2*mean(error(1:3,2));
     fopen(s)
@@ -30,6 +38,8 @@ if activate(2)
     
 end
 if activate(3)
+%CHANGE THIS COMPLETELY. IMPLEMENT LOGIC FROM THE ARDUINO INTERFACE CODE.
+
     while(rms(error(1:3,1))>threshold3)
     servo1=servo1+k3*mean(error(1:3,3));
     fopen(s)
