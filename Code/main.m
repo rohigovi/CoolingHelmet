7ba3514cebb12dfa20873becf9eaeedd3991e836
rohigovi <43045693+rohigovi@users.noreply.github.com>
2019-03-31 18:28:55 -0700

    Update main.m

--- Code/main.m
+++ Code/main.m
@@ -1,11 +1,19 @@
+
 close all
 instrreset
 clc
 function main(stop)
+% Desired temperature matrix, Should come from user input.
 desiredtempmat=25*ones(3,3);
 
-while ~stop    
+%Loop condition should be changed.
+while ~stop
+
 temp_mat=readTemperatureIdeal(0.1,false);
+%Tells you which of the three control systems need to be activated.
+activate=ChooseTube(temp_mat,desiredtempmat);
+
 LinearControl(temp_mat,desiredtempmat,false);
+
 end
 end
